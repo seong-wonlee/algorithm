a74b013810070c7218d8933de62d6edb0b311a29
seong-wonlee <jasmine1714@naver.com>
2023-02-15 20:01:10 +0900

    보충

---
+++ 20230214/복습.py
@@ -0,0 +1,34 @@
+# infix : 중위 표기식 문자열
+# n : 중위표기식 길이
+
+def get_postfix(infix, n):
+    # 결과로 나올 후위표기식
+    postfix = ""
+    stack = []
+    # 스택 밖에 있을 때 연산자의 우선순위
+    icp = {'+': 1, '*' : 2, '-' : 1, '/' : 2, '(': 3}
+    # 스택 안에 있을 때 연산자의 우선순위
+    isp = {'+': 1, '*': 2, '-': 1, '/': 2, '(': 0}
+
+    # 문자열의 길이만큼 반복
+    for i in range(n):
+        if "0" <= infix[i] <= "9":
+            postfix += infix[i]
+        else:
+            if infix[i] == ")":
+                char = stack.pop()
+                if char == "(":
+                    break
+                postfix += char
+            # 닫는 괄호가 아니라 기타 연산자
+            else:
+                # 현재 연산자의 우선순위보다 스택의 top에 있는 연산자의 우선순위가 같거나 높으면
+                while stack and isp[stack[-1]] >= icp[infix[i]]:
+                    postfix += stack.pop()
+                # 그게 아니면 스택에 연산자 추가
+                stack.append(infix[i])
+    while stack:    # 남아있는 연산자는 모두 출력
+        postfix += stack.pop()
+
+    return postfix
+
--- 20230215/계산기1.py
+++ 20230215/계산기1.py
@@ -1,34 +1,36 @@
-n = int(input())
 
-num = list(input())
-stack = []
-postfix = ''
-print(num)
-# 후위표기식으로 바꾸기
-for i in num:
-    # 숫자이면
-    if i.isdigit():
-        postfix += i
-    else: # 연산자가 나오면
-        if not stack:   # 스택이 비어있으면
-            stack.append(i)
-        else:   # 스택이 비어있지 않으면
-            postfix += stack.pop()
-            stack.append(i)
-    while stack:
+T = 10
+for tc in range(1, T+1):
+    n = int(input())
+    num = list(input())
+    stack = []
+    postfix = ''
+    # print(num)
+    # 후위표기식으로 바꾸기
+    for i in num:
+        # 숫자이면
+        if i.isdigit():
+            postfix += i
+        else: # 연산자가 나오면
+            if not stack:   # 스택이 비어있으면
+                stack.append(i)
+            else:   # 스택이 비어있지 않으면
+                postfix += stack.pop()
+                stack.append(i)
+    while stack:    # 스택에 연산자가 남아있으면
         postfix += stack.pop()
-print(postfix)
+    # print(postfix)
 
-# 후위 표기식 계산
-stack1 = []
-for i in postfix:
-    if i != '+':
-        stack1.append(int(i))
-    else:
-        b = stack1.pop()
-        a = stack1.pop()
-        stack1.append(b+a)
-print(stack1.pop())
+    # 후위 표기식 계산
+    stack1 = []
+    for i in postfix:
+        if i.isdigit():
+            stack1.append(int(i))
+        else:
+            b = stack1.pop()
+            a = stack1.pop()
+            stack1.append(b+a)
+    print(f'#{tc} {stack1[-1]}')
 
 
 
