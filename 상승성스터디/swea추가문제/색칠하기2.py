01761b1a5f4ec2b3cd0547cafd83a81d2af08ba1
seong-wonlee <jasmine1714@naver.com>
2023-03-10 17:50:49 +0900

    Sdd

--- 상승성스터디/swea추가문제/색칠하기2.py
+++ 상승성스터디/swea추가문제/색칠하기2.py
@@ -1,30 +1,27 @@
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    arr = [[0]*(11) for _ in range(11)]
+    arr = [[0]*(10) for _ in range(10)]
 
     didj = [(1,0),(-1,0),(0,-1),(0,1)]
     for i in range(N):
-        r1, c1, r2, c2, c = map(int, input().split())
-        for i in range(r1,r2+1):
+        r1, c1,r2, c2, c = map(int, input().split())
+        for k in range(r1,r2+1):
             for j in range(c1,c2+1):
-                arr[i][j] += c
-
-    # 겹쳐있는 부분을 0 으로 변환
-    for i in range(11):
-        for j in range(11):
-            if arr[i][j] == 3:
-                arr[i][j] = 0
+                arr[k][j] += c
+                # 겹쳐있는 부분을 0 으로 변환
+                if arr[k][j] == 3:
+                    arr[k][j] = 0
 
     ans = 0
-    for i in range(11):
-        for j in range(11):
+    for i in range(10):
+        for j in range(10):
             cnt = 0
             # 색이 칠해진 칸에 주위 살펴서
-            if arr[i][j] != 0:
+            if arr[i][j] == 1 or arr[i][j] == 2:
                 for k in range(4):
                     ni, nj = i + didj[k][0], j + didj[k][1]
-                    if 0 <= ni < 11 and 0 <= nj < 11:
+                    if 0 <= ni < 10 and 0 <= nj < 10:
                         # 주위에 0이 몇개인지 확인
                         if arr[ni][nj] == 0:
                             cnt += 1
